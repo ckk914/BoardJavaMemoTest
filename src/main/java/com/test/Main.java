2a9842b447061497745829ecce24897f08fc0b58
ckk914 <ckk9114@naver.com>
2024-05-29 16:38:42 +0900

    # 초기 세팅 = > Jsp popup

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,18 @@
+package com.test;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
+        SpringApplication.run(Main.class, args);
+
+    }
+}
