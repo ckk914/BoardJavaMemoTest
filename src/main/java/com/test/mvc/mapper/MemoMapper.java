aaad7e339e691e2ccfb7e494bf27f7490dcc71c3
ckk914 <ckk9114@naver.com>
2024-05-30 21:58:14 +0900

    # memo CRUD를 위한 인터페이스 설계 및 쿼리 설계

---
+++ src/main/java/com/test/mvc/mapper/MemoMapper.java
@@ -0,0 +1,18 @@
+package com.test.mvc.mapper;
+
+import com.test.mvc.dto.MemoPostDto;
+import com.test.mvc.entity.Memo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MemoMapper {
+    //전체 목록 조회
+    List<Memo> findAll();
+    //상세 조회
+//    Memo findOne(int memoNum);
+//    //메모 등록
+    boolean save(MemoPostDto memo);
+
+}
